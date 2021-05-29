725b4ff4eeacd7ebe4ab067ef46b26419dc4718a
doflavio <doflavio@gmail.com>
2021-05-28 22:02:51 -0300

    Hystrix fault tolerance

--- hr-worker/src/main/java/com/marcar/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/marcar/hrworker/resources/WorkerResource.java
@@ -2,6 +2,7 @@ package com.marcar.hrworker.resources;
 
 import java.util.List;
 
+import org.bouncycastle.crypto.RuntimeCryptoException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,6 +37,14 @@ public class WorkerResource {
 	
 	@GetMapping(value = "{id}")
 	public ResponseEntity <Worker> findById(@PathVariable Long id) {
+		
+		/*
+		try {
+			Thread.sleep(3000L);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		*/
 		
 		logger.info("PORT = " + env.getProperty("local.server.port"));
 		
